8f5f454a8ec862e0657ab68980333369b25fcaf2
Jens Troeger <jens.troeger@light-speed.de>
2019-04-18 06:09:49 +0300

    Add support for file types to setDocument().
    This is to implement access to all parameters of the document endpoint:
    
      https://github.com/jenstroeger/Bookalope/blob/master/API.md#post-bookflows-files-document

--- extensions/Bookalope/js/uses/BookalopeClient.js
+++ extensions/Bookalope/js/uses/BookalopeClient.js
@@ -1066,7 +1066,7 @@ Bookflow.prototype.getDocument = function() {
  * @returns {Promise}
  */
 
-Bookflow.prototype.setDocument = function(filename, file) {
+Bookflow.prototype.setDocument = function(filename, file, filetype="doc") {
   var bookflow = this;
   var bookalope = bookflow._bookalope;
 
@@ -1077,7 +1077,7 @@ Bookflow.prototype.setDocument = function(filename, file) {
       var url = bookflow.url + "/files/document";
       var params = {
         filename: filename,
-        filetype: "doc",
+        filetype: filetype,
         file: btoa(file)
       };
       bookalope.httpPOST(url, params)
